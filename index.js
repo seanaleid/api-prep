82e61595796cdd1ce9a45107361824e538b432fc
seanaleid <seanaleid@gmail.com>
2020-03-12 19:10:52 -0400

    Setup server and index
      * Adds functional code to index.js and server.js
      * Ther server runs

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-/* Go to https://www.youtube.com/watch?v=dQw4w9WgXcQ and follow the instructions and code! */
+const server = require('./server.js');
+const PORT = 8000;
+
+server.listen(PORT, () => {
+    console.log(`\n === The server is running! === \n`);
+});
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+
+
+
+server.get('/', (req, res) => {
+    res.status(200).json({ message: "The server is running 🤙!!"});
+});
+
+
+module.exports = server;
